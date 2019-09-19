8219f644cb086ce3e69efe404fa3f067bf669972
Alejandro-swd-qwerty <c.alejandro.83@hotmail.com>
2019-09-18 20:33:28 -0500

    [ADD] circle area

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
+/*  calculode edad en dias
+
+var age = 36;
+console.log("Los dias que has vivido son " + (36*365));*/
+
+/* Conversion entre tipos de moneda
+
 var pesos = 5.81 * 2500;
 var dollar = 0.052;
 var euros = 0.047;
 
 console.log("Si tengo 2500 Soles, tengo \n " + pesos + " pesos \n y "
-+ (pesos * dollar) + " dolares \n y " + (pesos * euros) + " euros" );
++ (pesos * dollar) + " dolares \n y " + (pesos * euros) + " euros" );*/
+
+var radius = 10; 
+const PI= 3.1416;
+var area = Pi * (radius*radius);
+
+console.log("El area del circulo es: " + area)
