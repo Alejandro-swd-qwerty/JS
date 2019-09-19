34e2e1dacc4b90b7b3c63deec0d7d85687c42b27
Alejandro-swd-qwerty <c.alejandro.83@hotmail.com>
2019-09-18 19:46:22 -0500

    [NEW] currency conversion

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
-var age = 36;
-console.log("Los dias que has vivido son " + (36*365));
+var pesos = 5.81 * 2500;
+var dollar = 0.052;
+var euros = 0.047;
+
+console.log("Si tengo 2500 Soles, tengo \n " + pesos + " pesos \n y "
++ (pesos * dollar) + " dolares \n y " + (pesos * euros) + " euros" );
