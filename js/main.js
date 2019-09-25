c648014d9d011eda93dbff722d7570c5c124e263
Alejandro-swd-qwerty <c.alejandro.83@hotmail.com>
2019-09-25 18:43:29 -0500

    [ADD] Adding palindrome

--- js/main.js
+++ js/main.js
@@ -11,6 +11,31 @@ var finder = array.forEach((element) => {
 
 */
 
+/*
+// Inverting a phrase
+
 var phrase = "hola amigos";
 
 var phraseInverter = (phrase) => phrase.split(" ").reverse().join(" ")
+*/
+
+
+var phrase = "anita lava la tina";
+
+var palindrome = phrase => {
+    let reversedPhrase;
+    let originalPhrase;
+   
+    originalPhrase = phrase.split(" ").join("");
+    reversedPhrase = originalPhrase.split("").reverse().join("");
+      
+    if (reversedPhrase === originalPhrase){
+        return "Es palindromo";
+    } else {
+        return "No es palindromo";
+    }
+
+}
+
+
+
